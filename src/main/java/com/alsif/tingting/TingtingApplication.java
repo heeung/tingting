b6e96e20ff4cacb9fa32e037e3132d4b0ee327bb
이찬희 <ych0716@naver.com>
2023-10-13 16:08:22 +0900

    init

---
+++ src/main/java/com/alsif/tingting/TingtingApplication.java
@@ -0,0 +1,13 @@
+package com.alsif.tingting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TingtingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TingtingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alsif/tingting/TingtingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alsif.tingting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TingtingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
