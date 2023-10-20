8b5a458d82c449521cc01e2abebb520f0bfe0421
heeung <skdi6031@naver.com>
2023-10-20 13:54:39 +0900

    feat: 카카오 로그인 구현 완료 & 상단바 생성

--- android/app/src/main/java/com/alsif/tingting/ui/login/LoginModalBottomSheet.kt
+++ android/app/src/main/java/com/alsif/tingting/ui/login/LoginModalBottomSheet.kt
@@ -7,32 +7,35 @@ import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import com.alsif.tingting.R
 import com.alsif.tingting.TingtingApp.DependencyContainer.authDataSource
+import com.alsif.tingting.base.BaseBottomSheet
 import com.alsif.tingting.data.local.AuthDataSource
 import com.alsif.tingting.databinding.BottomSheetLoginBinding
 import com.alsif.tingting.ui.main.MainActivityViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class LoginModalBottomSheet : BottomSheetDialogFragment()  {
+class LoginModalBottomSheet : BaseBottomSheet<BottomSheetLoginBinding>(BottomSheetLoginBinding::bind, R.layout.bottom_sheet_login)  {
     private val sharedViewModel: MainActivityViewModel by activityViewModels()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.bottom_sheet_login, container, false)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        binding.btnKakaoLogin.setOnClickListener {
-////            startKakaoLogin()
-//        }
+        binding.btnKakaoLogin.setOnClickListener {
+            kakaoLogin(
+                mActivity,
+                setAccessToken = {
+                    sharedViewModel.setAccessToken(it)
+                },
+                setRefreshToken = {
+                    sharedViewModel.setRefreshToken(it)
+                },
+                loginEvent = {
+                    sharedViewModel.loginEvent()
+                }
+            )
+        }
     }
 
     companion object {
-        const val TAG = "BasicBottomModalSheet"
+        const val TAG = "LoginBottomSheet"
     }
 }
--- android/app/src/main/java/com/alsif/tingting/ui/main/MainActivity.kt
+++ android/app/src/main/java/com/alsif/tingting/ui/main/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.alsif.tingting.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -15,13 +17,18 @@ import com.alsif.tingting.ui.login.LoginModalBottomSheet
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
+private const val TAG = "MainActivity"
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var navController: NavController
     lateinit var bottomNavigationView: BottomNavigationView
+    lateinit var loginBottomSheet: LoginModalBottomSheet
 
     private val sharedViewModel: MainActivityViewModel by viewModels()
 
@@ -29,6 +36,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         super.onCreate(savedInstanceState)
         initNavController()
         setBottomNavigationView()
+        subscribe()
     }
 
     private fun setBottomNavigationView(){
@@ -50,9 +58,35 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         navController.setGraph(graph, intent.extras)
     }
 
-    fun loginModalBottomSheet() {
-        val modal = LoginModalBottomSheet()
-        modal.show(supportFragmentManager, LoginModalBottomSheet.TAG)
+    fun showLoginBottomSheet() {
+        loginBottomSheet = LoginModalBottomSheet()
+        loginBottomSheet.show(supportFragmentManager, LoginModalBottomSheet.TAG)
+        subscribeLogin()
+    }
+
+    fun dismissLoginBottomSheet() {
+        loginBottomSheet.dismiss()
+    }
+
+    fun subscribeLogin() {
+        lifecycleScope.launch {
+            sharedViewModel.isLogined.collectLatest {
+                if (it) {
+                    Log.d(TAG, "subscribe: isLogined가 true로 바뀌었습니다.")
+                    dismissLoginBottomSheet()
+                }
+            }
+        }
+    }
+
+    fun subscribe() {
+        lifecycleScope.launch {
+            sharedViewModel.isLogined.collectLatest {
+                if (!it) {
+                    navController.navigate(R.id.homeFragment)
+                }
+            }
+        }
     }
 
     // boolean을 리턴함으로써 확장성 추가
@@ -61,11 +95,13 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
             null -> {
                 //  TODO api를 호출 하려나??
                 navController.popBackStack()
-                loginModalBottomSheet()
+                showLoginBottomSheet()
+                Log.d(TAG, "requireLogin: AccessToken 비어있습니다 -> ${sharedViewModel.getAccessToken()}")
                 false
             }
 
             else -> {
+                Log.d(TAG, "requireLogin: AccessToken 있습니다. -> ${sharedViewModel.getAccessToken()}")
                 true
             }
         }
