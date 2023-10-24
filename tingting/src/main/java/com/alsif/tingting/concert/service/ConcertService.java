868736f7b7e9ac6b80340acd9cf2ea905a3457f8
Hc-5514 <tbd05050@naver.com>
2023-10-24 13:24:55 +0900

    refact: 콘서트 찜 요청/취소 반환 로직 수정, 로그 수정

--- tingting/src/main/java/com/alsif/tingting/concert/service/ConcertService.java
+++ tingting/src/main/java/com/alsif/tingting/concert/service/ConcertService.java
@@ -59,13 +59,13 @@ public class ConcertService {
 			// 1) 예매 중: 예매 시작 시간 < 현재 시간 < 예매 종료 시간 (예매 오픈일 기준 내림차순)
 			if (orderBy.equals("now")) {
 				concertBaseDtos = concertRepository.findAllConcertNow(now, pageable);
-				log.info("예매 중인 콘서트 목록: {}", concertBaseDtos.toString());
+				log.info("예매 중인 콘서트 개수: {}", concertBaseDtos.getContent().size());
 				return returnConcertListResponseDto(requestDto, concertBaseDtos);
 			}
 			// 2) 예매 임박: 현재 시간 < 예매 오픈 시간 (예매 오픈일 기준 오름차순)
 			if (orderBy.equals("soon")) {
 				concertBaseDtos = concertRepository.findAllConcertSoon(now, pageable);
-				log.info("예매 임박 콘서트 목록: {}", concertBaseDtos.toString());
+				log.info("예매 임박 콘서트 개수: {}", concertBaseDtos.getContent().size());
 				return returnConcertListResponseDto(requestDto, concertBaseDtos);
 			}
 			throw new CustomException(ErrorCode.BAD_REQUEST_ORDERBY);
@@ -90,28 +90,28 @@ public class ConcertService {
 			// where 조건: 장소, 키워드, 시작일, 종료일
 			concertBaseDtos = concertRepository.findAllConcertByPlaceAndSearchWordAndDate(
 				place, searchWord, startDate, endDate, pageable);
-			log.info("콘서트 검색 결과 (장소, 키워드, 날짜): {}", concertBaseDtos.toString());
+			log.info("콘서트 검색 (장소, 키워드, 날짜), 검색된 콘서트 개수: {}", concertBaseDtos.getContent().size());
 			return returnConcertListResponseDto(requestDto, concertBaseDtos);
 		}
 		// 2. 장소 O && 키워드 O && (시작일 X || 종료일 X)
 		if (place != null && searchWord != null) {
 			// where 조건: 장소, 키워드
 			concertBaseDtos = concertRepository.findAllConcertByPlaceAndSearchWord(place, searchWord, pageable);
-			log.info("콘서트 검색 결과 (장소, 키워드): {}", concertBaseDtos.toString());
+			log.info("콘서트 검색 (장소, 키워드), 검색된 콘서트 개수: {}", concertBaseDtos.getContent().size());
 			return returnConcertListResponseDto(requestDto, concertBaseDtos);
 		}
 		// 3. 장소 O && 키워드 X && 시작일 O && 종료일 O
 		if (place != null && startDate != null && endDate != null) {
 			// where 조건: 장소, 시작일, 종료일
 			concertBaseDtos = concertRepository.findAllConcertByPlaceAndDate(place, startDate, endDate, pageable);
-			log.info("콘서트 검색 결과 (장소, 날짜): {}", concertBaseDtos.toString());
+			log.info("콘서트 검색 (장소, 날짜), 검색된 콘서트 개수: {}", concertBaseDtos.getContent().size());
 			return returnConcertListResponseDto(requestDto, concertBaseDtos);
 		}
 		// 4. 장소 O && 키워드 X && (시작일 X || 종료일 X)
 		if (place != null) {
 			// where 조건: 장소
 			concertBaseDtos = concertRepository.findAllConcertByPlace(place, pageable);
-			log.info("콘서트 검색 결과 (장소): {}", concertBaseDtos.toString());
+			log.info("콘서트 검색 (장소), 검색된 콘서트 개수: {}", concertBaseDtos.getContent().size());
 			return returnConcertListResponseDto(requestDto, concertBaseDtos);
 		}
 		// 5. 장소 X && 키워드 O && 시작일 O && 종료일 O
@@ -119,26 +119,26 @@ public class ConcertService {
 			// where 조건: 키워드, 시작일, 종료일
 			concertBaseDtos = concertRepository.findAllConcertBySearchWordAndDate(searchWord, startDate, endDate,
 				pageable);
-			log.info("콘서트 검색 결과 (키워드, 날짜): {}", concertBaseDtos.toString());
+			log.info("콘서트 검색 (키워드, 날짜), 검색된 콘서트 개수: {}", concertBaseDtos.getContent().size());
 			return returnConcertListResponseDto(requestDto, concertBaseDtos);
 		}
 		// 6. 장소 X && 키워드 O && (시작일 X || 종료일 X)
 		if (searchWord != null) {
 			// where 조건: 키워드
 			concertBaseDtos = concertRepository.findAllConcertBySearchWord(searchWord, pageable);
-			log.info("콘서트 검색 결과 (키워드): {}", concertBaseDtos.toString());
+			log.info("콘서트 검색 (키워드), 검색된 콘서트 개수: {}", concertBaseDtos.getContent().size());
 			return returnConcertListResponseDto(requestDto, concertBaseDtos);
 		}
 		// 7. 장소 X && 키워드 X && 시작일 O && 종료일 O
 		if (startDate != null && endDate != null) {
 			// where 조건: 시작일, 종료일
 			concertBaseDtos = concertRepository.findAllConcertByDate(startDate, endDate, pageable);
-			log.info("콘서트 검색 결과 (날짜): {}", concertBaseDtos.toString());
+			log.info("콘서트 검색 (날짜), 검색된 콘서트 개수: {}", concertBaseDtos.getContent().size());
 			return returnConcertListResponseDto(requestDto, concertBaseDtos);
 		}
 		// 8. 장소 X && 키워드 X && (시작일 X || 종료일 X): 예매 중인 콘서트 정보 그대로 준다.
 		concertBaseDtos = concertRepository.findAllConcertNow(now, pageable);
-		log.info("콘서트 검색 결과 (조건 없음): {}", concertBaseDtos.toString());
+		log.info("콘서트 검색 (조건 없음), 검색된 콘서트 개수: {}", concertBaseDtos.getContent().size());
 		return returnConcertListResponseDto(requestDto, concertBaseDtos);
 	}
 
@@ -147,13 +147,11 @@ public class ConcertService {
 	 */
 	private ConcertListResponseDto returnConcertListResponseDto(ConcertListRequestDto requestDto,
 		Page<ConcertBaseDto> concertBaseDtos) {
-		ConcertListResponseDto concertListResponseDto = ConcertListResponseDto.builder()
+		return ConcertListResponseDto.builder()
 			.totalPage(concertBaseDtos.getTotalPages())
 			.currentPage(requestDto.getCurrentPage())
 			.concerts(concertBaseDtos.getContent())
 			.build();
-		log.info("콘서트 목록: {}", concertListResponseDto);
-		return concertListResponseDto;
 	}
 
 	/*
@@ -204,10 +202,6 @@ public class ConcertService {
 				.build());
 		}
 
-		favorite = userConcertRepository.existsByUser_SeqAndConcert_Seq(
-			concertFavoriteRequestDto.getUserSeq(), concertFavoriteRequestDto.getConcertSeq());
-		log.info("favorite: {}", favorite);
-
-		return ConcertFavoriteResponseDto.builder().favorite(favorite).build();
+		return ConcertFavoriteResponseDto.builder().favorite(!favorite).build();
 	}
 }
