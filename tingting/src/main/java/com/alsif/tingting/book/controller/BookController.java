9fe3d9b1cecbfbf6db269894f418b51654f9b9d4
Hc-5514 <tbd05050@naver.com>
2023-11-02 09:40:45 +0900

    fix: 예매 요청 API 수정 (선택 좌석 PK를 Request Body 로 받기)

--- tingting/src/main/java/com/alsif/tingting/book/controller/BookController.java
+++ tingting/src/main/java/com/alsif/tingting/book/controller/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -136,7 +137,7 @@ public class BookController {
 	@PostMapping("/{concertDetailSeq}/seat")
 	ResponseEntity<SuccessResponseDto> book(
 		@PathVariable("concertDetailSeq") Long concertDetailSeq, @RequestParam Long userSeq,
-		ConcertSeatBookRequestDto requestDto) {
+		@RequestBody ConcertSeatBookRequestDto requestDto) {
 		log.info("===== 선택 좌석 예매 요청 시작, url={}, concertDetailSeq: {}, {} =====",
 			"/concerts", concertDetailSeq, requestDto.toString());
 
