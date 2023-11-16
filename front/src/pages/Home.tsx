aff917d760c27dfc21ae5204d82185b1cfa218cc
koobonjae <ninth6764@naver.com>
2023-11-16 09:35:25 +0900

    feat : 무한 스크롤 추가, 로그인 시 리코일에 유저이메일, 유저시퀀스 저장, 예약 페이지에서 홈 화면 이동 시 랜더링 안되는 현상 수정

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect, useRef, useCallback  } from 'react';
 import styles from './Home.module.css'
 import Search from '../components/search/Search.js'
 import ConcertList from '../components/concertlist/ConcertList.js'
@@ -8,52 +8,30 @@ import Lottie from 'lottie-react';
 import { animationLoading } from '../assets/Images/index.js';
 import { useNavigate } from 'react-router-dom'
 
-// import { useRecoilState } from 'recoil';
-// import { ConcertItemAtom } from '../recoil/ConcertItemAtom.tsx';
+
+import { useRecoilValue } from 'recoil';
+import { userEmailState, userSeqState } from "../recoil/UserAtom.tsx";
 import {
-    useQuery, 
+  useInfiniteQuery, 
   } from 'react-query';
 
 import axios from 'axios';
 import {API_BASE_URL} from '../constants/index.ts';
-// import {  BrowserRouter as Router, Route, Routes, Navigate  } from 'react-router-dom';
-
-
-// interface ConcertListProps {
-//     totalPage : number;
-//     currentPage : number;
-//     concerts: Concert[]
-// }
-
-// interface Concert {
-//     concertSeq : number;
-//     name : string;    
-//     holdOpenDate : string;
-//     holdCloseDate : string;
-//     imageUrl : string;
-//     concertHallName : string;
-//     concertHallCity : string;
-// }
-
-// interface ConcertListRequestDto {
-//     currentPage: number;
-//     itemCount: number;
-//     orderBy?: string;
-//     startDate?: string;
-//     endDate?: string;
-//     place?: string;
-//     searchWord?: string;
-// }
-
-
 
 function Home(){
 
+  const userEmail = useRecoilValue(userEmailState);
+  const userSeq = useRecoilValue(userSeqState);
+
+  console.log('userEmail:',userEmail)
+  console.log('userSeq:',userSeq)
+
   const navigate = useNavigate()
 
-  const [place,SetPlace] = useState("")
-  const [searchWord,SetSearchWord] = useState("")
-  const [isSearch,SetIsSearch] = useState(false)
+  const [maxPage,SetMaxPage] = useState(1)
+  const [place,SetPlace] = useState<string>("")
+  const [searchWord,SetSearchWord] = useState<string>("")
+  const [isSearch,SetIsSearch] = useState<boolean>(false)
 
   // const [nowPage,setNowPage] = useState(1)
   const [category,setCategory] = useState("now");
@@ -71,18 +49,17 @@ function Home(){
       SetIsSearch(false)
     } 
   },[isSearch])
+
   const search = () => {
     navigate(`/search`, { state: { place, searchWord } });
   };
-    // const navigate = useNavigate();
-    // const [concertList, setConcertList] = useRecoilState(ConcertItemAtom) 
 
     // API 호출 함수
-  const fetchData = async ({ pageParam=1 }) => {
+  const fetchData = async (page=1) => {
   
   const concertListRequest = {
-    currentPage: pageParam,
-    itemCount: 200,
+    currentPage: page,
+    itemCount: 60,
     orderBy : category
   }
 
@@ -90,16 +67,80 @@ function Home(){
   return response.data;
 };
 
-    const { isLoading, data } = useQuery([queryKey], ()=>fetchData({pageParam:1}), {
-        refetchOnWindowFocus: false, // react-query는 사용자가 사용하는 윈도우가 다른 곳을 갔다가 다시 화면으로 돌아오면 이 함수를 재실행합니다. 그 재실행 여부 옵션 입니다.
-        retry: 1, // 실패시 재호출 몇번 할지
-        // onSuccess: data => {
-        //   console.log(data);
-        // },
-        onError: e => {
-          console.log(e);
-        }
-      });
+
+const observerElem = useRef(null);
+
+  const { data:concertData, isLoading, hasNextPage, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery(
+      [queryKey],
+      ({ pageParam = 1 }) => fetchData(pageParam),
+      {
+        getNextPageParam: (_lastPage, pages) => {
+          if (pages.length < maxPage) {
+            return pages.length + 1;
+          } else return undefined;
+        },
+        onSuccess: res => {
+              SetMaxPage(res.pages[0].totalPage)
+            },
+      },
+      
+    );
+
+  const handleObserver = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      const [target] = entries;
+      if (target.isIntersecting && hasNextPage) {
+        fetchNextPage();
+      }
+    },
+    [fetchNextPage, hasNextPage]
+  );
+
+
+  useEffect(() => {
+    const element = observerElem.current;
+
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1,
+
+    };
+
+    const observer = new IntersectionObserver(handleObserver, options);
+    if (element) observer.observe(element);
+    return () => {
+      if (element) observer.unobserve(element);
+    };
+  }, [fetchNextPage, hasNextPage, handleObserver]);
+
+  const allPagesContent = concertData?.pages?.reduce(
+    (acc, page) => {
+      // 각 페이지의 내용을 합친다.
+      if (page && page.concerts) {
+        acc.concerts = acc.concerts.concat(page.concerts);
+      }
+      // 마지막 페이지의 totalPage와 currentPage만 유지한다.
+      if (page === concertData.pages[concertData.pages.length - 1]) {
+        acc.totalPage = page.totalPage;
+        acc.currentPage = page.currentPage;
+      }
+      return acc;
+    },
+    { concerts: [], totalPage: 0, currentPage: 0 } // 초기값 설정
+  );
+
+    // const { isLoading, data } = useQuery([queryKey], ()=>fetchData({pageParam:1}), {
+    //     refetchOnWindowFocus: false, // react-query는 사용자가 사용하는 윈도우가 다른 곳을 갔다가 다시 화면으로 돌아오면 이 함수를 재실행합니다. 그 재실행 여부 옵션 입니다.
+    //     retry: 1, // 실패시 재호출 몇번 할지
+    //     // onSuccess: data => {
+    //     //   console.log(data);
+    //     // },
+    //     onError: e => {
+    //       console.log(e);
+    //     }
+    //   });
 
     return(        
         <div
@@ -128,15 +169,26 @@ function Home(){
                 </div>        
 
                 {
-                  !isLoading && data?.concerts!==undefined && <ConcertList props={data} searchWord=""/>
+                  !isLoading && <ConcertList props={allPagesContent} searchWord=""/>
                 }
-                { (isLoading || data?.concerts===undefined) && 
+                { isLoading  && 
                 <div>
                   <Lottie 
                   className={styles["loading-box"]}
                   animationData={animationLoading}/>
                 </div>
                 }
+                <div className={styles["loader"]} ref={observerElem}>
+                {isFetchingNextPage && hasNextPage ?                 
+                <div>
+                  <Lottie 
+                  className={styles["loading-box"]}
+                  animationData={animationLoading}/>
+                </div> 
+                : 
+                <div>
+                </div>}
+              </div>
 
         </div>
     );
--- front/src/pages/Redirection.tsx
+++ front/src/pages/Redirection.tsx
@@ -1,41 +1,40 @@
 import { useEffect } from "react";
-// import axios from "axios"
-// import { API_BASE_URL,REST_API_KEY,REDIRECT_URI } from "../constants";
+import axios from "axios"
+import { API_BASE_URL } from "../constants";
+import { useSetRecoilState } from 'recoil';
+import { userEmailState, userSeqState } from "../recoil/UserAtom";
+import { useNavigate } from 'react-router-dom'
+
 
 const Redirection = () => {
-    useEffect(() => {
-        const params= new URL(document.location.toString()).searchParams;
-        const code = params.get('code');
-        // const grantType = "authorization_code"; 
-        // const REDIRECT_URI = 'http://k9d209.p.ssafy.io:9000/users/kakao';    
-        console.log(code)
 
-        // post로 /users/kakao에 code 전달하는 api 추가 아래 쪽에 작성한 api는 쓸모 X
-        // axios.post(
-        //     `https://kauth.kakao.com/oauth/token?grant_type=${grantType}&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${code}`,
-        //     {},
-        //     { headers: { "Content-type": "application/x-www-form-urlencoded;charset=utf-8" } }
-        // )
-        // .then((res: any) => {
-        //     console.log(res);
-        //     const { access_token } = res.data;
-        //     axios.post(
-        //         `https://kapi.kakao.com/v2/user/me`,
-        //         {},
-        //         {
-        //             headers: {
-        //                 Authorization: `Bearer ${access_token}`,
-        //                 "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-        //             }
-        //         }
-        //     )
-        //     .then((res: any) => {
-        //         console.log('2번쨰', res);
-        //     })
-        // })
-        // .catch((Error: any) => {
-        //     console.log(Error)
-        // })
+    const setUserEmail = useSetRecoilState(userEmailState);
+    const setUserSeq = useSetRecoilState(userSeqState);
+    const params= new URL(document.location.toString()).searchParams;
+    const code = params.get('code');
+
+    const navigate = useNavigate()
+    const goToOtherPage = (pageName:string) => {
+      navigate(`/${pageName}`);
+    }
+
+    const login = async () => {
+        await axios.post(`${API_BASE_URL}/users/kakao`, { code })
+            .then((res) => {
+                console.log(res);
+                setUserEmail(res.data.userEmail)
+                setUserSeq(res.data.userSeq)
+                goToOtherPage('')
+                
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
+
+    useEffect(() => {
+        login()
     }, [])
     
     return(
