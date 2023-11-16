56764e3bd4dc08066cca80830100a6010adead30
koobonjae <ninth6764@naver.com>
2023-11-16 12:45:05 +0900

    hotfix : tailwindCSS 적용

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -4,8 +4,8 @@ import flowbitePlugin from 'flowbite/plugin';
 
 const tailwindConfig = {
   content: [
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-    "./src/**/*.{html,js,jsx,ts,tsx}"
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
