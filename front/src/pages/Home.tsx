acf72126c43b865c1a83da6e5e6db87d134f5e92
koobonjae <ninth6764@naver.com>
2023-11-17 05:36:14 +0900

    fix : 에러 발생하는 부분 삭제

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -122,17 +122,6 @@ const observerElem = useRef(null);
     { concerts: [], totalPage: 0, currentPage: 0 } // 초기값 설정
   );
 
-    // const { isLoading, data } = useQuery([queryKey], ()=>fetchData({pageParam:1}), {
-    //     refetchOnWindowFocus: false, // react-query는 사용자가 사용하는 윈도우가 다른 곳을 갔다가 다시 화면으로 돌아오면 이 함수를 재실행합니다. 그 재실행 여부 옵션 입니다.
-    //     retry: 1, // 실패시 재호출 몇번 할지
-    //     // onSuccess: data => {
-    //     //   console.log(data);
-    //     // },
-    //     onError: e => {
-    //       console.log(e);
-    //     }
-    //   });
-
     return(        
         <div
         className={styles.container}>
