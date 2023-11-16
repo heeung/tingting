429921716881d193ed784a3d64936319464fb50e
koobonjae <ninth6764@naver.com>
2023-11-16 17:46:38 +0900

    feat : 리코일 초기값 수정

--- front/src/recoil/UserAtom.tsx
+++ front/src/recoil/UserAtom.tsx
@@ -2,10 +2,10 @@ import { atom } from "recoil";
 
 export const userEmailState = atom({
     key: 'userEmailState',
-    default: "knuee2014@naver.com", // 초기값은 로그인되어 있지 않음을 나타냅니다.
+    default: null, // 초기값은 로그인되어 있지 않음을 나타냅니다.
   });
   
   export const userSeqState = atom({
     key: 'userSeqState',
-    default: 675330,
+    default: null,
   });
